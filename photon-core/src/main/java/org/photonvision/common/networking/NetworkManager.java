d2fa14475f222a2faf5406dea26132113f58e346
Dave Stelts <stelts@live.com>
2023-02-12 23:18:31 -0500

    - disabled code to set hostname and network stuff

--- photon-core/src/main/java/org/photonvision/common/networking/NetworkManager.java
+++ photon-core/src/main/java/org/photonvision/common/networking/NetworkManager.java
@@ -47,101 +47,101 @@ public class NetworkManager {
 
         var config = ConfigManager.getInstance().getConfig().getNetworkConfig();
         logger.info("Setting " + config.connectionType + " with team team " + config.teamNumber);
-        if (Platform.isLinux()) {
-            if (!Platform.isRoot()) {
-                logger.error("Cannot manage hostname without root!");
-            }
+        // if (Platform.isLinux()) {
+        //     if (!Platform.isRoot()) {
+        //         logger.error("Cannot manage hostname without root!");
+        //     }
 
-            // always set hostname
-            if (config.hostname.length() > 0) {
-                try {
-                    var shell = new ShellExec(true, false);
-                    shell.executeBashCommand("cat /etc/hostname | tr -d \" \\t\\n\\r\"");
-                    var oldHostname = shell.getOutput().replace("\n", "");
+        //     // always set hostname
+        //     if (config.hostname.length() > 0) {
+        //         try {
+        //             var shell = new ShellExec(true, false);
+        //             shell.executeBashCommand("cat /etc/hostname | tr -d \" \\t\\n\\r\"");
+        //             var oldHostname = shell.getOutput().replace("\n", "");
 
-                    var setHostnameRetCode =
-                            shell.executeBashCommand(
-                                    "echo $NEW_HOSTNAME > /etc/hostname".replace("$NEW_HOSTNAME", config.hostname));
-                    setHostnameRetCode =
-                            shell.executeBashCommand("hostnamectl set-hostname " + config.hostname);
+        //             var setHostnameRetCode =
+        //                     shell.executeBashCommand(
+        //                             "echo $NEW_HOSTNAME > /etc/hostname".replace("$NEW_HOSTNAME", config.hostname));
+        //             setHostnameRetCode =
+        //                     shell.executeBashCommand("hostnamectl set-hostname " + config.hostname);
 
-                    // Add to /etc/hosts
-                    var addHostRetCode =
-                            shell.executeBashCommand(
-                                    String.format(
-                                            "sed -i \"s/127.0.1.1.*%s/127.0.1.1\\t%s/g\" /etc/hosts",
-                                            oldHostname, config.hostname));
+        //             // Add to /etc/hosts
+        //             var addHostRetCode =
+        //                     shell.executeBashCommand(
+        //                             String.format(
+        //                                     "sed -i \"s/127.0.1.1.*%s/127.0.1.1\\t%s/g\" /etc/hosts",
+        //                                     oldHostname, config.hostname));
 
-                    shell.executeBashCommand("sudo service avahi-daemon restart");
+        //             shell.executeBashCommand("sudo service avahi-daemon restart");
 
-                    var success = setHostnameRetCode == 0 && addHostRetCode == 0;
-                    if (!success) {
-                        logger.error(
-                                "Setting hostname returned non-zero codes (hostname/hosts) "
-                                        + setHostnameRetCode
-                                        + "|"
-                                        + addHostRetCode
-                                        + "!");
-                    } else {
-                        logger.info("Set hostname to " + config.hostname);
-                    }
-                } catch (Exception e) {
-                    logger.error("Failed to set hostname!", e);
-                }
+        //             var success = setHostnameRetCode == 0 && addHostRetCode == 0;
+        //             if (!success) {
+        //                 logger.error(
+        //                         "Setting hostname returned non-zero codes (hostname/hosts) "
+        //                                 + setHostnameRetCode
+        //                                 + "|"
+        //                                 + addHostRetCode
+        //                                 + "!");
+        //             } else {
+        //                 logger.info("Set hostname to " + config.hostname);
+        //             }
+        //         } catch (Exception e) {
+        //             logger.error("Failed to set hostname!", e);
+        //         }
 
-            } else {
-                logger.warn("Got empty hostname?");
-            }
+        //     } else {
+        //         logger.warn("Got empty hostname?");
+        //     }
 
-            if (config.connectionType == NetworkMode.DHCP) {
-                var shell = new ShellExec();
-                try {
-                    // set nmcli back to DHCP, and re-run dhclient -- this ought to grab a new IP address
-                    shell.executeBashCommand(
-                            config.setDHCPcommand.replace(
-                                    NetworkConfig.NM_IFACE_STRING, config.networkManagerIface));
-                    shell.executeBashCommand("dhclient " + config.physicalInterface, false);
-                } catch (Exception e) {
-                    logger.error("Exception while setting DHCP!");
-                }
-            } else if (config.connectionType == NetworkMode.STATIC) {
-                var shell = new ShellExec();
-                if (config.staticIp.length() > 0) {
-                    try {
-                        shell.executeBashCommand(
-                                config
-                                        .setStaticCommand
-                                        .replace(NetworkConfig.NM_IFACE_STRING, config.networkManagerIface)
-                                        .replace(NetworkConfig.NM_IP_STRING, config.staticIp));
+        //     if (config.connectionType == NetworkMode.DHCP) {
+        //         var shell = new ShellExec();
+        //         try {
+        //             // set nmcli back to DHCP, and re-run dhclient -- this ought to grab a new IP address
+        //             shell.executeBashCommand(
+        //                     config.setDHCPcommand.replace(
+        //                             NetworkConfig.NM_IFACE_STRING, config.networkManagerIface));
+        //             shell.executeBashCommand("dhclient " + config.physicalInterface, false);
+        //         } catch (Exception e) {
+        //             logger.error("Exception while setting DHCP!");
+        //         }
+        //     } else if (config.connectionType == NetworkMode.STATIC) {
+        //         var shell = new ShellExec();
+        //         if (config.staticIp.length() > 0) {
+        //             try {
+        //                 shell.executeBashCommand(
+        //                         config
+        //                                 .setStaticCommand
+        //                                 .replace(NetworkConfig.NM_IFACE_STRING, config.networkManagerIface)
+        //                                 .replace(NetworkConfig.NM_IP_STRING, config.staticIp));
 
-                        if (Platform.isRaspberryPi()) {
-                            // Pi's need to manually have their interface adjusted?? and the 5 second sleep is
-                            // integral in my testing (Matt)
-                            shell.executeBashCommand(
-                                    "sh -c 'nmcli con down "
-                                            + config.networkManagerIface
-                                            + "; nmcli con up "
-                                            + config.networkManagerIface
-                                            + "'");
-                        } else {
-                            // for now just bring down /up -- more testing needed on beelink et al
-                            shell.executeBashCommand(
-                                    "sh -c 'nmcli con down "
-                                            + config.networkManagerIface
-                                            + "; nmcli con up "
-                                            + config.networkManagerIface
-                                            + "'");
-                        }
-                    } catch (Exception e) {
-                        logger.error("Error while setting static IP!", e);
-                    }
-                } else {
-                    logger.warn("Got empty static IP?");
-                }
-            }
-        } else {
-            logger.info("Not managing network on non-Linux platforms");
-        }
+        //                 if (Platform.isRaspberryPi()) {
+        //                     // Pi's need to manually have their interface adjusted?? and the 5 second sleep is
+        //                     // integral in my testing (Matt)
+        //                     shell.executeBashCommand(
+        //                             "sh -c 'nmcli con down "
+        //                                     + config.networkManagerIface
+        //                                     + "; nmcli con up "
+        //                                     + config.networkManagerIface
+        //                                     + "'");
+        //                 } else {
+        //                     // for now just bring down /up -- more testing needed on beelink et al
+        //                     shell.executeBashCommand(
+        //                             "sh -c 'nmcli con down "
+        //                                     + config.networkManagerIface
+        //                                     + "; nmcli con up "
+        //                                     + config.networkManagerIface
+        //                                     + "'");
+        //                 }
+        //             } catch (Exception e) {
+        //                 logger.error("Error while setting static IP!", e);
+        //             }
+        //         } else {
+        //             logger.warn("Got empty static IP?");
+        //         }
+        //     }
+        // } else {
+        //     logger.info("Not managing network on non-Linux platforms");
+        // }
     }
 
     public void reinitialize() {
